ba5b15e6ccd83ec5bcd4617fb65516391c8fade9
Vlad Yankovenko <ipgost09@icloud.com>
2019-04-04 15:34:22 +0300

    Added CustomLoader to true directory

---
+++ KudaGo/Loader/CustomLoader.swift
@@ -0,0 +1,49 @@
+//
+//  CustomLoader.swift
+//
+//
+//  Created by Влад Янковенко on 30.03.2019.
+//
+
+import UIKit
+
+class CustomLoader: UIView {
+    static let instance = CustomLoader()
+    
+    //let loaderImage = UIImage(named: "loader")
+    
+    
+    
+    lazy var fullView: UIView = {
+        let fullView  = UIView(frame: UIScreen.main.bounds)
+        fullView.backgroundColor = UIColor.white
+        fullView.isUserInteractionEnabled = false
+        return fullView
+    }()
+    
+    
+    lazy var loader: UIView = {
+        let loader  = UIImageView(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        loader.center = fullView.center
+        loader.isUserInteractionEnabled = false
+        loader.image = UIImage(named: "loader")
+        let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 1
+        rotation.isCumulative = true
+        rotation.repeatCount = Float.greatestFiniteMagnitude
+        loader.layer.add(rotation, forKey: "rotationAnimation")
+        return loader
+    }()
+    
+    func showLoader() {
+        self.addSubview(fullView)
+        self.fullView.addSubview(loader)
+        self.fullView.bringSubviewToFront(self.loader)
+        UIApplication.shared.keyWindow?.addSubview(fullView)
+    }
+    
+    func hidesLoader(){
+        self.fullView.removeFromSuperview()
+    }
+}
